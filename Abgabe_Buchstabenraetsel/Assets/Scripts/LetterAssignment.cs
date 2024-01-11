80b15e1fe26ac8ec7450ade8f4ef6a5de902fa43
Natsume <natsumeniichan@gmail.com>
2024-01-11 16:03:16 +0100

    Letters working
    more or less...

--- Abgabe_Buchstabenraetsel/Assets/Scripts/LetterAssignment.cs
+++ Abgabe_Buchstabenraetsel/Assets/Scripts/LetterAssignment.cs
@@ -2,14 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class LetterAssignment : MonoBehaviour
+public class LetterAssignment : MonoBehaviour, IPointerDownHandler
 {
+    private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public bool WordOne;
+    public bool WordTwo;
+    public bool WordThree;
+    public bool isFinished = false;
+    public bool isClicked = false;
+
     private TMP_Text letter;
+    public Image Image;
+    private PuzzleManager puzzleScript;
 
     private void Awake()
     {
-        letter = GetComponent<TMP_Text>();
-        letter.text = Random.Range(0, 25);
+        Image = GetComponent<Image>();
+        puzzleScript = FindObjectOfType<PuzzleManager>();
+
+        if (WordOne == false && WordTwo == false && WordThree == false)
+        {
+            letter = GetComponentInChildren<TMP_Text>();
+            letter.text = alphabet[Random.Range(0, alphabet.Length)].ToString();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (isClicked == true)
+        {
+            return;
+        }
+
+        if (WordOne == true)
+        {
+            Image.color = Color.green;
+            puzzleScript.RightLetter(gameObject);
+        }
+
+        else if (WordTwo == true)
+        {
+            Image.color = Color.green;
+            puzzleScript.RightLetter(gameObject);
+        }
+
+        else if (WordThree == true)
+        {
+            Image.color = Color.green;
+            puzzleScript.RightLetter(gameObject);
+        }
+
+        else
+        {
+            Image.color = Color.red;
+            puzzleScript.WrongLetter(gameObject);
+
+            Invoke("Reset", 0.5f);
+        }
+    }
+
+    private void Reset()
+    {
+        if (isFinished == false)
+        {
+            Image.color = Color.white;
+        }
+
+        puzzleScript.ResetLetters();
     }
 }
--- Abgabe_Buchstabenraetsel/Assets/Scripts/PuzzleManager.cs
+++ Abgabe_Buchstabenraetsel/Assets/Scripts/PuzzleManager.cs
@@ -1,16 +1,135 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class PuzzleManager : MonoBehaviour
 {
-    public GameObject Letter;
-    //private TMP_Text text;
+    private AudioSource rightSound;
+    private AudioSource wrongSound;
+
+    public int letterOne;
+    public int letterTwo;
+    public int letterThree;
+    [SerializeField] private int words;
+
+    private LetterAssignment letterScript;
+
+    [SerializeField] private List<GameObject> wordOne;
+    [SerializeField] private List<GameObject> wordTwo;
+    [SerializeField] private List<GameObject> wordThree;
 
     private void Awake()
     {
-        //text = Letter.GetComponentInChildren<TMP_Text>();
-        //text = Random.Range(0, 26);
+        rightSound = GameObject.Find("AudioManager W").GetComponent<AudioSource>();
+        wrongSound = GameObject.Find("AudioManager R").GetComponent<AudioSource>();
+    }
+
+    public void RightLetter(GameObject Letter)
+    {
+        letterScript = Letter.GetComponent<LetterAssignment>();
+
+        rightSound.Play();
+        letterScript.isClicked = true;
+
+        if (letterScript.WordOne == true)
+        {
+            letterOne++;
+
+            wordOne.Add(Letter);
+
+            if (letterOne >= 3)
+            {
+                words++;
+
+                foreach (GameObject letter in wordOne)
+                {
+                    letter.GetComponent<LetterAssignment>().isFinished = true;
+                }
+            }
+        }
+
+        if (letterScript.WordTwo == true)
+        {
+            letterTwo++;
+
+            wordTwo.Add(Letter);
+
+            if (letterTwo >= 4)
+            {
+                words++;
+
+                foreach (GameObject letter in wordTwo)
+                {
+                    letter.GetComponent<LetterAssignment>().isFinished = true;
+                }
+            }
+        }
+
+        if (letterScript.WordThree == true)
+        {
+            letterThree++;
+
+            wordThree.Add(Letter);
+
+            if (letterThree >= 5)
+            {
+                words++;
+
+                foreach (GameObject letter in wordThree)
+                {
+                    letter.GetComponent<LetterAssignment>().isFinished = true;
+                }
+            }
+        }
+
+    }
+
+    public void WrongLetter(GameObject Letter)
+    {
+        letterScript = Letter.GetComponent<LetterAssignment>();
+
+        wrongSound.Play();
+    }
+
+    public void ResetLetters()
+    {
+        foreach (GameObject letter in wordOne)
+        {
+            if (letter.GetComponent<LetterAssignment>().isFinished == false)
+            {
+                letter.GetComponent<Image>().color = Color.white;
+                letter.GetComponent<LetterAssignment>().isClicked = false;
+                letterOne--;
+
+                wordOne.Remove(letter);
+            }
+        }
+
+        foreach (GameObject letter in wordTwo)
+        {
+            if (letter.GetComponent<LetterAssignment>().isFinished == false)
+            {
+                letter.GetComponent<Image>().color = Color.white;
+                letter.GetComponent<LetterAssignment>().isClicked = false;
+                letterTwo--;
+
+                wordOne.Remove(letter);
+            }
+        }
+
+        foreach (GameObject letter in wordThree)
+        {
+            if (letter.GetComponent<LetterAssignment>().isFinished == false)
+            {
+                letter.GetComponent<Image>().color = Color.white;
+                letter.GetComponent<LetterAssignment>().isClicked = false;
+                letterThree--;
+
+                wordOne.Remove(letter);
+            }
+        }
     }
 }
